a67024b054147b8e0be01e8b071fa3219fce0c0e
JohnnyBackus <john.p.backus@gmail.com>
2023-10-31 13:50:17 -0700

    created stores with Constructor

--- js/app.js
+++ js/app.js
@@ -1,156 +1,173 @@
-const Seattle = {
-  hoursOpen: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
-  phoneNumber: '123-456-7890',
-  address: '2901 3rd Ave #300, Seattle, WA 98121',
-  minCustomer: 23,
-  maxCustomer: 65,
-  avgCookieSale: 6.3,
-  hourlySales: [],
-  getRandomHourlyCookieSales: function() {
-    for (let i=0; i <this.hoursOpen.length; i++) {
-      let hourCookieSales = Math.floor(Math.random() * (this.maxCustomer - this.minCustomer + 1) + this.minCustomer) * this.avgCookieSale;
-      this.hourlySales.push(Math.floor(hourCookieSales));
-    };
-  },
-  dailyCookieSales: function() {
-    let sumSales = 0;
-    for (let i=0; i <this.hourlySales.length; i++) {
-      sumSales += this.hourlySales[i];
-    };
-    return sumSales;
-  },
-}
+// const Seattle = {
+//   hoursOpen: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
+//   phoneNumber: '123-456-7890',
+//   address: '2901 3rd Ave #300, Seattle, WA 98121',
+//   minCustomer: 23,
+//   maxCustomer: 65,
+//   avgCookieSale: 6.3,
+//   hourlySales: [],
+//   getRandomHourlyCookieSales: function() {
+//     for (let i=0; i <this.hoursOpen.length; i++) {
+//       let hourCookieSales = Math.floor(Math.random() * (this.maxCustomer - this.minCustomer + 1) + this.minCustomer) * this.avgCookieSale;
+//       this.hourlySales.push(Math.floor(hourCookieSales));
+//     };
+//   },
+//   dailyCookieSales: function() {
+//     let sumSales = 0;
+//     for (let i=0; i <this.hourlySales.length; i++) {
+//       sumSales += this.hourlySales[i];
+//     };
+//     return sumSales;
+//   },
+// }
 
-Seattle.getRandomHourlyCookieSales();
-Seattle.dailyCookieSales();
-console.log (Seattle.hourlySales);
-console.log (Seattle.dailyCookieSales());
-
-
-const Tokyo = {
-  hoursOpen: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
-  phoneNumber: '123-456-7890',
-  address: '2901 3rd Ave #300, Seattle, WA 98121',
-  minCustomer: 3,
-  maxCustomer: 24,
-  avgCookieSale: 1.2,
-  hourlySales: [],
-  getRandomHourlyCookieSales: function() {
-    for (let i=0; i <this.hoursOpen.length; i++) {
-      let hourCookieSales = Math.floor(Math.random() * (this.maxCustomer - this.minCustomer + 1) + this.minCustomer) * this.avgCookieSale;
-      this.hourlySales.push(Math.floor(hourCookieSales));
-    };
-  },
-  dailyCookieSales: function() {
-    let sumSales = 0;
-    for (let i=0; i <this.hourlySales.length; i++) {
-      sumSales += this.hourlySales[i];
-    };
-    return sumSales;
-  },
-}
-
-Tokyo.getRandomHourlyCookieSales();
-console.log (Tokyo.hourlySales);
-console.log (Tokyo.dailyCookieSales());
+// Seattle.getRandomHourlyCookieSales();
+// Seattle.dailyCookieSales();
+// console.log (Seattle.hourlySales);
+// console.log (Seattle.dailyCookieSales());
 
 
-const Dubai = {
-  hoursOpen: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
-  phoneNumber: '123-456-7890',
-  address: '2901 3rd Ave #300, Seattle, WA 98121',
-  minCustomer: 11,
-  maxCustomer: 38,
-  avgCookieSale: 3.7,
-  hourlySales: [],
-  getRandomHourlyCookieSales: function() {
-    for (let i=0; i <this.hoursOpen.length; i++) {
-      let hourCookieSales = Math.floor(Math.random() * (this.maxCustomer - this.minCustomer + 1) + this.minCustomer) * this.avgCookieSale;
-      this.hourlySales.push(Math.floor(hourCookieSales));
-    };
-  },
-  dailyCookieSales: function() {
-    let sumSales = 0;
-    for (let i=0; i <this.hourlySales.length; i++) {
-      sumSales += this.hourlySales[i];
-    };
-    return sumSales;
-  },
-}
+// const Tokyo = {
+//   hoursOpen: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
+//   phoneNumber: '123-456-7890',
+//   address: '2901 3rd Ave #300, Seattle, WA 98121',
+//   minCustomer: 3,
+//   maxCustomer: 24,
+//   avgCookieSale: 1.2,
+//   hourlySales: [],
+//   getRandomHourlyCookieSales: function() {
+//     for (let i=0; i <this.hoursOpen.length; i++) {
+//       let hourCookieSales = Math.floor(Math.random() * (this.maxCustomer - this.minCustomer + 1) + this.minCustomer) * this.avgCookieSale;
+//       this.hourlySales.push(Math.floor(hourCookieSales));
+//     };
+//   },
+//   dailyCookieSales: function() {
+//     let sumSales = 0;
+//     for (let i=0; i <this.hourlySales.length; i++) {
+//       sumSales += this.hourlySales[i];
+//     };
+//     return sumSales;
+//   },
+// }
 
-Dubai.getRandomHourlyCookieSales();
-console.log (Dubai.hourlySales);
-console.log (Dubai.dailyCookieSales());
-
-
-const Paris = {
-  hoursOpen: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
-  phoneNumber: '123-456-7890',
-  address: '2901 3rd Ave #300, Seattle, WA 98121',
-  minCustomer: 20,
-  maxCustomer: 38,
-  avgCookieSale: 2.3,
-  hourlySales: [],
-  getRandomHourlyCookieSales: function() {
-    for (let i=0; i <this.hoursOpen.length; i++) {
-      let hourCookieSales = Math.floor(Math.random() * (this.maxCustomer - this.minCustomer + 1) + this.minCustomer) * this.avgCookieSale;
-      this.hourlySales.push(Math.floor(hourCookieSales));
-    };
-  },
-  dailyCookieSales: function() {
-    let sumSales = 0;
-    for (let i=0; i <this.hourlySales.length; i++) {
-      sumSales += this.hourlySales[i];
-    };
-    return sumSales;
-  },
-}
-
-Paris.getRandomHourlyCookieSales();
-console.log (Paris.hourlySales);
-console.log (Paris.dailyCookieSales());
+// Tokyo.getRandomHourlyCookieSales();
+// console.log (Tokyo.hourlySales);
+// console.log (Tokyo.dailyCookieSales());
 
 
-const Lima = {
-  hoursOpen: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
-  phoneNumber: '123-456-7890',
-  address: '2901 3rd Ave #300, Seattle, WA 98121',
-  minCustomer: 2,
-  maxCustomer: 16,
-  avgCookieSale: 4.6,
-  hourlySales: [],
-  getRandomHourlyCookieSales: function() {
-    for (let i=0; i <this.hoursOpen.length; i++) {
-      let hourCookieSales = Math.floor(Math.random() * (this.maxCustomer - this.minCustomer + 1) + this.minCustomer) * this.avgCookieSale;
-      this.hourlySales.push(Math.floor(hourCookieSales));
-    };
-  },
-  dailyCookieSales: function() {
-    let sumSales = 0;
-    for (let i=0; i <this.hourlySales.length; i++) {
-      sumSales += this.hourlySales[i];
-    };
-    return sumSales;
-  },
-}
+// const Dubai = {
+//   hoursOpen: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
+//   phoneNumber: '123-456-7890',
+//   address: '2901 3rd Ave #300, Seattle, WA 98121',
+//   minCustomer: 11,
+//   maxCustomer: 38,
+//   avgCookieSale: 3.7,
+//   hourlySales: [],
+//   getRandomHourlyCookieSales: function() {
+//     for (let i=0; i <this.hoursOpen.length; i++) {
+//       let hourCookieSales = Math.floor(Math.random() * (this.maxCustomer - this.minCustomer + 1) + this.minCustomer) * this.avgCookieSale;
+//       this.hourlySales.push(Math.floor(hourCookieSales));
+//     };
+//   },
+//   dailyCookieSales: function() {
+//     let sumSales = 0;
+//     for (let i=0; i <this.hourlySales.length; i++) {
+//       sumSales += this.hourlySales[i];
+//     };
+//     return sumSales;
+//   },
+// }
 
-Lima.getRandomHourlyCookieSales();
-console.log (Lima.hourlySales);
-console.log (Lima.dailyCookieSales());
+// Dubai.getRandomHourlyCookieSales();
+// console.log (Dubai.hourlySales);
+// console.log (Dubai.dailyCookieSales());
 
 
-// const areaSales = document.getElementById("root");
+// const Paris = {
+//   hoursOpen: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
+//   phoneNumber: '123-456-7890',
+//   address: '2901 3rd Ave #300, Seattle, WA 98121',
+//   minCustomer: 20,
+//   maxCustomer: 38,
+//   avgCookieSale: 2.3,
+//   hourlySales: [],
+//   getRandomHourlyCookieSales: function() {
+//     for (let i=0; i <this.hoursOpen.length; i++) {
+//       let hourCookieSales = Math.floor(Math.random() * (this.maxCustomer - this.minCustomer + 1) + this.minCustomer) * this.avgCookieSale;
+//       this.hourlySales.push(Math.floor(hourCookieSales));
+//     };
+//   },
+//   dailyCookieSales: function() {
+//     let sumSales = 0;
+//     for (let i=0; i <this.hourlySales.length; i++) {
+//       sumSales += this.hourlySales[i];
+//     };
+//     return sumSales;
+//   },
+// }
 
-// const seattleSales = document.createElement("section");
-// areaSales.appendChild(seattleSales);
-// const seattleSalesHeader = document.createElement("h2");
-// seattleSalesHeader.textContent = 'Seattle';
-// seattleSales.appendChild(seattleSalesHeader);
-// const seattleSalesList = document.createElement("ul");
-// seattleSalesList.textcontent = `${Seattle.hoursOpen}: ${Seattle.hourlySales}`
-// seattleSales.appendChild(seattleSalesList);
+// Paris.getRandomHourlyCookieSales();
+// console.log (Paris.hourlySales);
+// console.log (Paris.dailyCookieSales());
 
-// function createHourlySalesList(cityhoursOpen , citygetRandomHourlyCookieSales, citydailyCookieSales) {
+
+// const Lima = {
+//   hoursOpen: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
+//   phoneNumber: '123-456-7890',
+//   address: '2901 3rd Ave #300, Seattle, WA 98121',
+//   minCustomer: 2,
+//   maxCustomer: 16,
+//   avgCookieSale: 4.6,
+//   hourlySales: [],
+//   getRandomHourlyCookieSales: function() {
+//     for (let i=0; i <this.hoursOpen.length; i++) {
+//       let hourCookieSales = Math.floor(Math.random() * (this.maxCustomer - this.minCustomer + 1) + this.minCustomer) * this.avgCookieSale;
+//       this.hourlySales.push(Math.floor(hourCookieSales));
+//     };
+//   },
+//   dailyCookieSales: function() {
+//     let sumSales = 0;
+//     for (let i=0; i <this.hourlySales.length; i++) {
+//       sumSales += this.hourlySales[i];
+//     };
+//     return sumSales;
+//   },
+// }
+
+// Lima.getRandomHourlyCookieSales();
+// console.log (Lima.hourlySales);
+// console.log (Lima.dailyCookieSales());
+
+
+// // const areaSales = document.getElementById("root");
+
+// // const seattleSales = document.createElement("section");
+// // areaSales.appendChild(seattleSales);
+// // const seattleSalesHeader = document.createElement("h2");
+// // seattleSalesHeader.textContent = 'Seattle';
+// // seattleSales.appendChild(seattleSalesHeader);
+// // const seattleSalesList = document.createElement("ul");
+// // seattleSalesList.textcontent = `${Seattle.hoursOpen}: ${Seattle.hourlySales}`
+// // seattleSales.appendChild(seattleSalesList);
+
+// // function createHourlySalesList(cityhoursOpen , citygetRandomHourlyCookieSales, citydailyCookieSales) {
+// //   const ul = document.createElement('ul');
+// //   for (let i = 0; i < cityhoursOpen.length; i++) {
+// //     const li = document.createElement('li');
+// //     li.textContent = `${cityhoursOpen[i]}: ${cityhourlySales[i]}`;
+// //     ul.appendChild(li);
+// //   };
+// //   const li = document.createElement('li');
+// //   li.textContent = `Total: ${citydailyCookieSales}`;
+// //   ul.appendChild(li);
+// //   return ul;
+// // }
+// // const seattleSales = document.getElementById("seattleSales");
+// // const hourlySales = createHourlySalesList(Seattle.hoursOpen, Seattle[getRandomHourlyCookieSales], Seattle.dailyCookieSales);
+// // seattleSales.appendChild(hourlySales);
+
+
+// function createHourlySalesList(cityhoursOpen , cityhourlySales, citydailyCookieSales) {
 //   const ul = document.createElement('ul');
 //   for (let i = 0; i < cityhoursOpen.length; i++) {
 //     const li = document.createElement('li');
@@ -161,45 +178,82 @@ console.log (Lima.dailyCookieSales());
 //   li.textContent = `Total: ${citydailyCookieSales}`;
 //   ul.appendChild(li);
 //   return ul;
-// }
+// };
+
 // const seattleSales = document.getElementById("seattleSales");
-// const hourlySales = createHourlySalesList(Seattle.hoursOpen, Seattle[getRandomHourlyCookieSales], Seattle.dailyCookieSales);
-// seattleSales.appendChild(hourlySales);
+// const seattleHourlySalesList = createHourlySalesList(Seattle.hoursOpen, Seattle.hourlySales, Seattle.dailyCookieSales());
+// seattleSales.appendChild(seattleHourlySalesList);
 
+// const tokyoSales = document.getElementById("tokyoSales");
+// const tokyoHourlySalesList = createHourlySalesList(Tokyo.hoursOpen, Tokyo.hourlySales, Tokyo.dailyCookieSales());
+// tokyoSales.appendChild(tokyoHourlySalesList);
 
-function createHourlySalesList(cityhoursOpen , cityhourlySales, citydailyCookieSales) {
-  const ul = document.createElement('ul');
-  for (let i = 0; i < cityhoursOpen.length; i++) {
-    const li = document.createElement('li');
-    li.textContent = `${cityhoursOpen[i]}: ${cityhourlySales[i]}`;
-    ul.appendChild(li);
+// const dubaiSales = document.getElementById("dubaiSales");
+// const dubaiHourlySalesList = createHourlySalesList(Dubai.hoursOpen, Dubai.hourlySales, Dubai.dailyCookieSales());
+// dubaiSales.appendChild(dubaiHourlySalesList);
+
+// const parisSales = document.getElementById("parisSales");
+// const parisHourlySalesList = createHourlySalesList(Paris.hoursOpen, Paris.hourlySales, Paris.dailyCookieSales());
+// parisSales.appendChild(parisHourlySalesList);
+
+// const limaSales = document.getElementById("limaSales");
+// const limaHourlySalesList = createHourlySalesList(Lima.hoursOpen, Lima.hourlySales, Lima.dailyCookieSales());
+// limaSales.appendChild(limaHourlySalesList);
+
+function Store(city, hoursOpen, phoneNumber, address, minCustomer, maxCustomer, avgCookieSale) {
+  this.city = city;
+  this.hoursOpen = hoursOpen;
+  this.phoneNumber = phoneNumber;
+  this.address = address;
+  this.minCustomer = minCustomer;
+  this.maxCustomer = maxCustomer;
+  this.avgCookieSale = avgCookieSale;
+  this.hourlySales = [];
+  this.dailySales = undefined;
+  this.getRandomHourlySales = function () {
+    for (let i=0; i <this.hoursOpen.length; i++) {
+      let hourCookieSales = Math.floor(Math.random() * (this.maxCustomer - this.minCustomer + 1) + this.minCustomer) * this.avgCookieSale;
+      this.hourlySales.push(Math.floor(hourCookieSales));
+    };
   };
-  const li = document.createElement('li');
-  li.textContent = `Total: ${citydailyCookieSales}`;
-  ul.appendChild(li);
-  return ul;
+  this.totalDailyCookieSales = function() {
+    let sumSales = 0;
+    for (let i=0; i <this.hourlySales.length; i++) {
+    sumSales += this.hourlySales[i];
+    };
+    this.dailySales = sumSales;
+  };
 };
 
-const seattleSales = document.getElementById("seattleSales");
-const seattleHourlySalesList = createHourlySalesList(Seattle.hoursOpen, Seattle.hourlySales, Seattle.dailyCookieSales());
-seattleSales.appendChild(seattleHourlySalesList);
 
-const tokyoSales = document.getElementById("tokyoSales");
-const tokyoHourlySalesList = createHourlySalesList(Tokyo.hoursOpen, Tokyo.hourlySales, Tokyo.dailyCookieSales());
-tokyoSales.appendChild(tokyoHourlySalesList);
+const seattleStore = new Store('Seattle', ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'], '123-456-7890', '2901 3rd Ave #300, Seattle, WA 98121', 23, 65, 6.3);
+seattleStore.getRandomHourlySales();
+seattleStore.totalDailyCookieSales();
+console.log(seattleStore);
 
-const dubaiSales = document.getElementById("dubaiSales");
-const dubaiHourlySalesList = createHourlySalesList(Dubai.hoursOpen, Dubai.hourlySales, Dubai.dailyCookieSales());
-dubaiSales.appendChild(dubaiHourlySalesList);
+const tokyoStore = new Store('Tokyo', ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'], '123-456-7890', '2901 3rd Ave #300, Seattle, WA 98121', 3, 24, 1.2);
+tokyoStore.getRandomHourlySales();
+tokyoStore.totalDailyCookieSales();
+console.log(tokyoStore);
 
-const parisSales = document.getElementById("parisSales");
-const parisHourlySalesList = createHourlySalesList(Paris.hoursOpen, Paris.hourlySales, Paris.dailyCookieSales());
-parisSales.appendChild(parisHourlySalesList);
+const dubaiStore = new Store('Dubai', ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'], '123-456-7890', '2901 3rd Ave #300, Seattle, WA 98121', 3, 24, 1.2);
+dubaiStore.getRandomHourlySales();
+dubaiStore.totalDailyCookieSales();
+console.log(dubaiStore);
 
-const limaSales = document.getElementById("limaSales");
-const limaHourlySalesList = createHourlySalesList(Lima.hoursOpen, Lima.hourlySales, Lima.dailyCookieSales());
-limaSales.appendChild(limaHourlySalesList);
+const parisStore = new Store('Paris', ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pmparispm', '5pm', '6pm', '7pm'], '123-456-7890', '2901 3rd Ave #300, Seattle, WA 98121', 3, 24, 1.2);
+parisStore.getRandomHourlySales();
+parisStore.tparisailyCookieSales();
+console.log(parisStore);
 
-function render(city) {
+const limaStore = new Store('Lima', ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'], '123-456-7890', '2901 3rd Ave #300, Seattle, WA 98121', 3, 24, 1.2);
+limaStore.getRandomHourlySales();
+limaStore.totalDailyCookieSales();
+console.log(limaStore);
 
-}
+Store.prototype.renderTable = function(cityStore) {
+  const tableBody = document.getElementById("salesTable");
+  const tableRows = createchild("tr");
+  for(let i=0; i<this.hoursOpen.length; i++);
+};
+
