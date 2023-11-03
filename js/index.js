f9b6ed5183e74e1853ee8cbb0477dd2c92a87067
JohnnyBackus <john.p.backus@gmail.com>
2023-11-02 18:51:57 -0700

    took a towel to some code; it still works. Commit now before I break it

---
+++ js/index.js
@@ -0,0 +1,20 @@
+function renderStoreInfo() {
+  let storeInfoColumn = document.getElementById("storeInfo");
+  for (let i = 0; i < allStores.length; i++) {
+    let individualStoreInfo = document.createElement("div");
+      storeInfoColumn.appendChild(individualStoreInfo);
+    let storeCity = document.createElement("h2");
+      storeCity.textContent = `${allStores[i].city}`;
+      individualStoreInfo.appendChild(storeCity);
+    let storeHours = document.createElement("p");
+      storeHours.textContent = `Hours Open: ${allStores[i].hoursOpen[0]}-${allStores[i].hoursOpen.slice(-1)[0]}`;//cool trick ChatGPT taught me for accessing the last index value
+      individualStoreInfo.appendChild(storeHours);
+    let storePhone = document.createElement("p");
+      storePhone.textContent = `Contact Info: ${allStores[i].phoneNumber}`;
+      individualStoreInfo.appendChild(storePhone);
+    let storeLocation = document.createElement("p");
+      storeLocation.textContent = `Location: ${allStores[i].address}`;
+      individualStoreInfo.appendChild(storeLocation);
+  };
+};
+renderStoreInfo();
--- js/sales.js
+++ js/sales.js
@@ -19,8 +19,6 @@ Store.prototype.renderTable = function() {
 
 };
 
-
-// Later, I can create a single renderTable function with the allStores array and a for loop.
 seattleStore.renderTable();
 dubaiStore.renderTable();
 tokyoStore.renderTable();
@@ -29,6 +27,10 @@ limaStore.renderTable();
 
 function createTableFooter () {
   let table = document.getElementById("salesTable");
+  let existingTableFooter = table.querySelector("tfoot");
+    if (existingTableFooter) {
+      table.removeChild(existingTableFooter);
+    };
   let tableFooter = document.createElement("tfoot");
   table.appendChild(tableFooter);
   let footerRow = document.createElement("tr");
@@ -65,7 +67,7 @@ createTableFooter();
 let storeForm = document.getElementById("storeForm");
 let inputs = document.querySelectorAll("input");
 let newStoreEntry = {};
-
+const storeLocation = document.getElementById("city");
 
 for( let i = 0; i < inputs.length; i++ ) {
   inputs[i].addEventListener("change", function(event) {
@@ -74,61 +76,16 @@ for( let i = 0; i < inputs.length; i++ ) {
   });
 }
 
-function NewStore(location, minCust, maxCust, avgCookie) {
-  this.city = location;
-  this.hoursOpen = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
-  this.phoneNumber = '';
-  this.address = '';
-  this.minCustomer = minCust;
-  this.maxCustomer = maxCust;
-  this.avgCookieSale = avgCookie;
-  this.hourlySales = [];
-  this.dailySales = undefined;
-  this.getRandomHourlySales = function () {
-    for (let i=0; i <this.hoursOpen.length; i++) {
-      let hourCookieSales = Math.floor(Math.random() * (this.maxCustomer - this.minCustomer + 1) + this.minCustomer) * this.avgCookieSale;
-      this.hourlySales.push(Math.floor(hourCookieSales));
-    };
-  };
-  this.totalDailyCookieSales = function() {
-    let sumSales = 0;
-    for (let i=0; i <this.hourlySales.length; i++) {
-      sumSales += this.hourlySales[i];
-    };
-    this.dailySales = sumSales;
-  };
-  allStores.push(this);
-};
-console.log(allStores);
-// renderNewStoreRow = function() {
-//   let tableBody = document.getElementById("salesTableBody");
-//   let tableRow = document.createElement("tr");
-//   tableBody.appendChild(tableRow);
-
-//   let locationCell = document.createElement("td");
-//   locationCell.textContent = `${this.city}`;
-//   tableRow.appendChild(locationCell);
-
-//   for (let i = 0; i < 14; i++) {
-//     let hourlySaleCell = document.createElement("td");
-//     hourlySaleCell.textContent = `${this.hourlySales[i]}`;
-//     tableRow.appendChild(hourlySaleCell);
-//   };
-
-//   let locationTotalCell = document.createElement("td");
-//   locationTotalCell.textContent = `${this.dailySales}`;
-//   tableRow.appendChild(locationTotalCell);
-
-// };
-
 storeForm.addEventListener("submit", function(event) {
   event.preventDefault();
-  console.log(newStoreEntry);
-  let store = new NewStore( newStoreEntry.location, newStoreEntry.minCust, newStoreEntry.maxCust, newStoreEntry.avgCookie);
-  allStores.push(store);
-  console.log(allStores);
-  allStores[5].renderTable();
-//   renderNewStoreRow();
-//   createTableFooter();
+  storeForm.reset();
+  storeLocation.focus();
+  // console.log(newStoreEntry);
+  let store = new Store( newStoreEntry.city, ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'], "", "",newStoreEntry.minCust, newStoreEntry.maxCust, newStoreEntry.avgCookie);
+  store.getRandomHourlySales();
+  store.totalDailyCookieSales();
+  store.renderTable();
+  // console.log(allStores);
+  createTableFooter();
   }
 );
